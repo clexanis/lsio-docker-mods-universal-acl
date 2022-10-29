26fc460a4cf67f5528aa04a833a18c454130e24c
clexanis <clexanis@users.noreply.github.com>
2022-10-30 00:16:34 +0200

    Nothing to see here!

--- root/defaults/mods-universal-acl-disable-chown-data.sh
+++
@@ -1,8 +0,0 @@
-# Part of the LinuxServer.io acl universal mod
-# This function disallow chown of any path containing "/data"
-chown() {
-	if [[ $* = *"/data"* ]]; then
-		printf "Warning: ACL mod activated, chown of /data folder disallowed."
-		exit 1; else
-		"chown $*"; fi
-}
