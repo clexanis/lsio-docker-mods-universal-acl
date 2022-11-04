d84c95ce2bea567cc33b67f8ca3327864315d5a3
clexanis <clexanis@users.noreply.github.com>
2022-11-04 19:19:22 +0100

    Add a newline for good figure

--- root/defaults/chown-shim.sh
+++ root/defaults/chown-shim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [[ $* = *"/data"* ]]; then
-	printf "Warning: ACL mod activated, chown of /data folders disallowed."
+	printf "Warning: ACL mod activated, chown of /data folders disallowed.\n"
 	exit 1; else
 	"$(which chown)-noshim $*"; fi
