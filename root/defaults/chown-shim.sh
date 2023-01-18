a642c50656ea8cd255913c9c8901c1808551ae4b
clexanis <clexanis@users.noreply.github.com>
2023-01-18 20:47:38 +0100

    Fix v2

--- root/defaults/chown-shim.sh
+++ root/defaults/chown-shim.sh
@@ -2,4 +2,4 @@
 if [[ $* = *"/data"* ]]; then
 	printf "Warning: ACL mod activated, chown of /data folders disallowed.\n"
 	exit 1; else
-	"$(which chown)-noshim $*"; fi
+	"$(which chown-noshim) $*"; fi
